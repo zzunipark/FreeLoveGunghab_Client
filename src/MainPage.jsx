f685fa44d7b5addf99e9aad69eacfe4527bc4cfb
zzunipark <gusoong0829@naver.com>
2024-10-29 21:52:54 +0900

    Initial Commit

---
+++ src/MainPage.jsx
@@ -0,0 +1,15 @@
+import topimage from './img/Top.png';
+import topsearch from './img/TopSearch.png'
+
+function MainPage() {
+
+    return (
+        <>
+            <img src={topimage} width = '100%'/>
+            <img src={topsearch} width = '100%'/>
+        </>
+    )
+
+}
+
+export default MainPage;
---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+        background-color: #f5f5f5;
+    }
+`;
+
+export default GlobalStyles;
